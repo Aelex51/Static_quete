4ed0a410a6e9c98fa740abce5319cda87de36455
Alexandre Delmotte <113446625+Aelex51@users.noreply.github.com>
2022-11-07 23:12:29 +0100

    Add files via upload

---
+++ Static/Speedometer.php
@@ -0,0 +1,18 @@
+<?php
+
+class Speedometer
+{
+    public const converter = 1.609344;
+
+    public static function convertMilesToKm(int $speed)
+    {
+        $finalSpeed = $speed * self::converter;
+        return round($finalSpeed, 2);
+    }
+
+    public static function convertKmToMiles(int $speed)
+    {
+       $finalSpeed = $speed / self::converter;
+       return round($finalSpeed,2);
+    }
+}
